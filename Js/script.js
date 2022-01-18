11b24fc7f1eb4af87696c14fee027e171ad5455e
Michael-Musili <93195066+Michael-Musili@users.noreply.github.com>
2022-01-18 14:34:18 +0300

    added Js functions

--- Js/script.js
+++ Js/script.js
@@ -1,25 +1,45 @@
-$(document).ready(function() {
-    $("#Aboutus").submit(function(event) {
-        
-        function flavor() {
-          var pizzaFlavour = document.getElementById("flavor").value;
-          return parseInt(pizzaFlavour);
-        }
-        function size() {
-          var pizzaSize = document.getElementById("size").value;
-          return parseInt(pizzaSize);
-        }
-        function crust() {
-          var pizzaCrust = document.getElementById("crust").value;
-          return parseInt(pizzaCrust);
-        }
-        function topping() {
-          var pizzaTopping = document.getElementById("topping").value;
-          return parseInt(pizzaTopping);
-        }
-        function number() {
-          var pizzaNumber = document.getElementById("number").value;
-          return parseInt(pizzaNumber);
-        }
-})
-});
+let flavourPrice, crustPrice,toppingPrice,sizePrice ;
+function newFlavour(){
+  if (document.getElementById("flavour").value == meat-pizza){
+    flavourprice = 1500;
+  }
+  else if(document.getElementById("flavour").value == cheese-pizza) {
+    flavourprice=1500;
+  }
+  else if  (document.getElementById("flavour").value == cheese-pizza){
+    flavourprice=1500;
+  }
+  else if (document.getElementById("flavour").value == hawaii-pizza){
+    flavourprice=1000;
+  }
+  
+  else (document.getElementById("flavour").value == peperoni-pizza)
+    flavourprice=1600;
+    return(flavorPrice)
+  };
+  
+  function newCrust (){
+  if (document.getElementById("crust").value == crispy ){
+    crustPrice=200;
+  }
+  else if  (document.getElementById("crust").value == stuffed ){
+    crustPrice=400;
+  }
+ else  (document.getElementById("crust").value == gluton )
+    crustPrice=300;
+    return(crustPrice)
+  };
+function newTopping (){
+  if   (document.getElementById("topping").value == sausage ){
+    toppingPrice=150;
+  }
+  else if (document.getElementById("topping").value == beef ){
+    toppingPrice=350;
+  }
+  else if (document.getElementById("topping").value == bacon ){
+    toppingPrice=300;
+  }
+  else  (document.getElementById("topping").value == ham )
+  toppingPrice=200;
+  return(toppingPrice)
+};
